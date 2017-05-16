962b5852ecc81e06fa105713074940b581c0f51b
Jason Terk <jason@goterkyourself.com>
2017-05-16 14:53:06 -0700

    Fix flake8 Errors

--- mail/lib/offlineimap-helpers.py
+++ mail/lib/offlineimap-helpers.py
@@ -1,14 +1,17 @@
-import os
-import re
-import subprocess
-
 # offlineimap-helpers.py includes the `get_password' function that will pull
 # IMAP/SMTP passwords from the OS X keychain; they no longer need to be
 # specified in plaintext. To use this feature the "name" of the keychain item
 # should be the SMTP server for the account; the "kind" should be "Internet
 # password", the "account" should be the account, the "where" should be
 # "smtp://SMTP_SERVER".
+import subprocess
+
+
 def get_password(account, server):
-    devnull = os.open('/dev/null', os.O_WRONLY)
-    password = subprocess.check_output(["security", "find-internet-password", "-a", account, "-s", server, "-w"])
+    password = subprocess.check_output(
+        ["security",
+         "find-internet-password",
+         "-a", account,
+         "-s", server, "-w"]
+    )
     return password
